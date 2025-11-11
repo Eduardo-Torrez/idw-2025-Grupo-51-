cc3fd0153f45e2956fce6c1bd66f22d5ae187f54
TorresMariana <mt.grafico@gmail.com>
2025-11-11 00:07:48 -0300

    javascript de turnos

--- js/utils.js
+++ js/utils.js
@@ -7,3 +7,7 @@ export function botonCerrar(elemento){
     }
 };
 window.botonCerrar = botonCerrar;
+
+
+export const diasdelasemana = ['Domingo','Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado'];
+export const horarios = ['08:00','09:00','10:00','11:00','12:00','16:00','17:00','18:00','19:00','20:00'];
