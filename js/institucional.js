a86ebba438cd32cddd999e4704c8e12bfdcc4f77
EduardoTorrez <eduardotorrez0595@gmail.com>
2025-11-13 10:28:54 -0300

    se arreglo el login y intitucional

---
+++ js/institucional.js
@@ -0,0 +1,25 @@
+import { obtenerDatos } from "./localstorage.js";
+let medicos = obtenerDatos("medicos");
+let especialidades =obtenerDatos("especialidades");
+
+function mostrarlista(){
+    let listaMedicosContainer = document.getElementById("listaMedicosCcontainer");
+    listaMedicosContainer.innerHTML='';
+    medicos.forEach((medico)=>{
+        console.log(medico)
+        let cardHTML = document.createElement('div');
+        cardHTML.innerHTML= `
+            <div class="col d-flex">
+                <div class="card shadow-sm h-100 w-100">
+                    <img src="${medico.fotografia}" class="card-img-top" alt="Foto de medico" style="height: 250px; object-fit: cover;">
+                    <div class="card-body text-start">
+                        <h5 class="card-title">${medico.nombre} ${medico.apellido}</h5>
+                        <p class="card-text"><strong>Especialidad:</strong> ${especialidades.find(especialidad=>especialidad.id===medico.especialidad).nombre}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+        listaMedicosContainer.appendChild(cardHTML);
+    })
+}
+mostrarlista();
